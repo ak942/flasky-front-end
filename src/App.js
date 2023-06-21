4e2371cc60146d07cedc42549c4a80f9febaf728
Agnes Kim <agneskim4@gmail.com>
2023-06-21 10:34:40 -0400

    new form to add a new crystal

--- src/App.js
+++ src/App.js
@@ -1,51 +1,66 @@
 import './App.css';
 import CrystalList from './components/CrystalList';
 import React, {useState} from 'react';
+import axios from 'axios';
+import CrystalForm from './components/CrystalForm'
 const crystalsData = [
-  {
-    id: 1,
-    name: "Ameythest",
-    color: "purple",
-    powers: "Infinite Knowledge and Wisdom",
-    charges: 0
-  },
-  {
-    id: 2,
-    name: "Tiger's Eye",
-    color: "orange",
-    powers: "confidence and strength",
-    charges: 0
-  },
-  {
-    id: 3,
-    name: "Rose Quartz",
-    color: "pink",
-    powers: "love",
-    charges: 0
-  }
+  // {
+  //   id: 1,
+  //   name: "Ameythest",
+  //   color: "purple",
+  //   powers: "Infinite Knowledge and Wisdom",
+  //   charges: 0
+  // },
+  // {
+  //   id: 2,
+  //   name: "Tiger's Eye",
+  //   color: "orange",
+  //   powers: "confidence and strength",
+  //   charges: 0
+  // },
+  // {
+  //   id: 3,
+  //   name: "Rose Quartz",
+  //   color: "pink",
+  //   powers: "love",
+  //   charges: 0
+  // }
 ]
 //this is jsx it eventuals turns into js
 function App() { //this is an app component that holds other components
   const [crystals, setCrystals] = useState(crystalsData)
   const title = "The Crystal Cove"
 
+  React.useEffect(()=> {
+    axios.get('http://localhost:5000/crystals').then((response)=> {
+      setCrystals(response.data)
+    })
+  },[])
+
   const increaseCharge=(id) => {
       //crystals is being passed into prevCrystals 
       //needs to retun a brand new list to detect the comparison
-      setCrystals(prevCrystals => {
-        const updatedCrystal = prevCrystals.map(crystal => {
-          return crystal.id ===id ? {...crystal, charges: crystal.charges+1} : crystal 
+      axios.patch(`http://localhost:5000/crystals/${id}`).then(response => {
+        setCrystals(prevCrystals => {
+          const updatedCrystal = prevCrystals.map(crystal => {
+            return crystal.id ===id ? response.data : crystal 
+          })
+          // return entry.id === id ?{...entry, liked: !entry.liked} : entry
+        return updatedCrystal
         })
-      return updatedCrystal
       })
   };
+  //{...crystal, charges: crystal.charges+1} means returning the dict with updated charges
   //function to remove crystal being passed down into crystallist/crystal
   const removeCrystal = (id)=> {
+    axios.delete(`http://localhost:5000/crystals/${id}`).then(() => {
       setCrystals(prevCrystals => {
         const updatedCrystals = prevCrystals.filter(crystal => crystal.id !== id)
         return updatedCrystals
       })
+    })
   };
+  //the function will now update the UI to reflect the change 
 
   //change the total on the front page
   const totalCharges = () => {
@@ -65,6 +80,7 @@ function App() { //this is an app component that holds other components
     <main classname='App'>
         <h1>{title}</h1>
         <button onClick ={showAllCrystal}> Show All Crystals </button>
+        <CrystalForm/> 
         <p> Total Charges: {totalCharges()} </p>
         <CrystalList crystals = {crystals} increaseCharge ={increaseCharge} removeCrystal={removeCrystal} />
     </main>
---
+++ src/components/CrystalForm.js
@@ -0,0 +1,71 @@
+import { getValue } from '@testing-library/user-event/dist/utils';
+import React from 'react';
+
+const CrystalForm = () => {
+    //instead of seperate states you can have one
+    // const [name, setName] = React.useState('')
+    // const [color, setColor] = React.useState(' ')
+    // const [powers, setPower] =React.useState(' ')
+    const [formFields, setFormFields] = React.useState({
+        name: "",
+        color: "",
+        powers: "",
+        charges: null
+    })
+
+    const handleChange = event => setFormFields({
+        ...formFields,
+        [event.target.name]: event.target.value
+    })
+
+    // const onNameChange =(event) => {
+    //     const {value} = event.target;
+    //     setFormFields({
+    //         ...formFields,
+    //         name: value
+    //     });
+    // };
+
+    // const onColorChange = (event)=> {
+    //     setFormFields({
+    //         ...formFields,
+    //         color: event.target.value
+    //     });
+    // };
+
+    // const onPowersChange = (event)=> {
+    //     setFormFields({
+    //         ...formFields,
+    //         powers: event.target.va
+    //     });    
+    // };
+
+    return (
+    <form>
+        <section>
+            <h2>Add a Crystal</h2>
+            <div className="new_crystal_fields">
+                <div>
+                    <label htmlFor="name">Name: </label>
+                    <input name="name" value={formFields.name} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="color"> Color: </label>
+                    <input name="color" value ={formFields.CrystalFormcolor} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="power"> Powers: </label>
+                    <input name="powers" value = {formFields.powers} onChange={handleChange}/>
+                </div>
+                <div>
+                    <label htmlFor="Charge"> Charges: </label>
+                    <input name="Charges" value = {formFields.powers} onChange={handleChange}/>
+                </div>
+                <button className="new-crystal-button" type="submit" value="Add Crystal">Add Crystal</button>
+            </div>
+        </section>
+    </form>
+    );
+}
+
+export default CrystalForm;
